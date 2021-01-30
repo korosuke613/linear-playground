7b3262d80bacaaef4c379b240179e0e6a4c518ae
Futa Hirakoba <futa-hirakoba@cybozu.co.jp>
2021-01-31 02:55:05 +0900

    chore: experiment webhooks

---
+++ webhook/src/samples/createComment.js
@@ -0,0 +1,24 @@
+exports.createComment = {
+  action: "create",
+  createdAt: "2021-01-30T11:47:17.226Z",
+  data: {
+    id: "5081a82b-23c0-4b30-838c-37d7be02a506",
+    createdAt: "2021-01-30T11:47:17.226Z",
+    updatedAt: "2021-01-30T11:47:17.226Z",
+    body: "コメントだーい",
+    userId: "80e102b0-4a45-44d4-93a8-044bcfb4cd39",
+    issueId: "ac36bcc2-c24e-42d6-8d7e-3e13107f89be",
+    issue: {
+      id: "ac36bcc2-c24e-42d6-8d7e-3e13107f89be",
+      title: "webhook test 2",
+    },
+    user: {
+      id: "80e102b0-4a45-44d4-93a8-044bcfb4cd39",
+      name: "futa-hirakoba@cybozu.co.jp",
+    },
+    reactions: [],
+  },
+  url:
+    "https://linear.app/korosuke613/issue/KOR-12/webhook-test-2#comment-5081a82b",
+  type: "Comment",
+};
